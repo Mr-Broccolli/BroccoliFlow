0bfa2455f8079294b19f963d9a6852f256a7ffb3
Mr Broccoli <bhatnagar.nitigya13btt@gmail.com>
2026-06-14 00:28:33 +0530

    add categories

---
+++ config.py
@@ -0,0 +1,41 @@
+VERSION = "1.6.0"
+
+FILE_CATEGORIES = {
+    "Images": [
+        ".png", ".jpg", ".jpeg", ".gif",
+        ".webp", ".bmp", ".tiff", ".svg",
+        ".ico"
+    ],
+
+    "Documents": [
+        ".pdf", ".doc", ".docx",
+        ".txt", ".rtf",
+        ".xls", ".xlsx", ".csv",
+        ".ppt", ".pptx",
+        ".odt", ".ods", ".odp"
+    ],
+
+    "Videos": [
+        ".mp4", ".mkv", ".avi",
+        ".mov", ".wmv", ".flv",
+        ".webm", ".m4v"
+    ],
+
+    "Audio": [
+        ".mp3", ".wav", ".flac",
+        ".aac", ".ogg", ".m4a",
+        ".wma"
+    ],
+
+    "Archives": [
+        ".zip", ".rar", ".7z",
+        ".tar", ".gz", ".bz2",
+        ".xz"
+    ],
+
+    "Installation Media": [
+        ".exe", ".msi", ".msix",
+        ".msixbundle", ".appx",
+        ".appxbundle"
+    ]
+}
