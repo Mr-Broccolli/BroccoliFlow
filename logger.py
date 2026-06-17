d5a3e1e971b90e61da43f193ee4c7bfb92247c5b
Mr Broccoli <bhatnagar.nitigya13btt@gmail.com>
2026-06-17 23:38:03 +0530

    Implement v1.8.0: make logger

---
+++ logger.py
@@ -0,0 +1,15 @@
+import logging
+from pathlib import Path
+
+LOG_DIR = Path("logs")
+LOG_FILE = LOG_DIR / "broccoliflow.log"
+
+def setup_logger():
+    LOG_DIR.mkdir(exist_ok=True)
+    logging.basicConfig(
+        filename=LOG_FILE,
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+    return logging.getLogger("BroccoliFlow")
