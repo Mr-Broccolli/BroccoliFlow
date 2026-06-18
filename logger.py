174b52cb90212069afc568c0f9359cf182a927f2
Mr Broccoli <bhatnagar.nitigya13btt@gmail.com>
2026-06-18 22:43:55 +0530

    Add debug info

--- logger.py
+++ logger.py
@@ -4,12 +4,16 @@ from pathlib import Path
 LOG_DIR = Path("logs")
 LOG_FILE = LOG_DIR / "broccoliflow.log"
 
-def setup_logger():
+def setup_logger(debug=False):
     LOG_DIR.mkdir(exist_ok=True)
     
     logger = logging.getLogger("BroccoliFlow")
+    
+    # Set level based on debug toggle
+    level = logging.DEBUG if debug else logging.INFO
+    logger.setLevel(level)
+    
     if not logger.handlers:
-        logger.setLevel(logging.INFO)
         file_handler = logging.FileHandler(LOG_FILE)
         formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
         file_handler.setFormatter(formatter)
@@ -17,5 +21,7 @@ def setup_logger():
         
     return logger
 
-
-logger = setup_logger()
+logger = setup_logger(debug=False)
+# Add this function to logger.py
+def set_debug_level(debug):
+    logger.setLevel(logging.DEBUG if debug else logging.INFO)
